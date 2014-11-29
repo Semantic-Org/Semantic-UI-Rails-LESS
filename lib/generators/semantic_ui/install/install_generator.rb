d00073ca152c33256bd5aabe9c22cdaa947a447b
Maxim Dobryakov <maxim.dobryakov@gmail.com>
2014-11-29 20:37:28 +0300

    Try to fix `no implicit conversion of nil into String` on build server

--- lib/generators/semantic_ui/install/install_generator.rb
+++ lib/generators/semantic_ui/install/install_generator.rb
@@ -5,15 +5,10 @@ module SemanticUi
 
     desc 'Install Semantic UI assets to application'
 
-    cattr_reader :javascripts do
-      File.join('vendor', 'assets', 'javascripts', 'semantic_ui')
-    end
-
-    cattr_reader :stylesheets do
-      File.join('vendor', 'assets', 'stylesheets', 'semantic_ui')
-    end
-
     def install_semantic_ui
+      javascripts = File.join('vendor', 'assets', 'javascripts', 'semantic_ui')
+      stylesheets = File.join('vendor', 'assets', 'stylesheets', 'semantic_ui')
+
       copy_file 'semantic_ui.js', File.join(javascripts, 'semantic_ui.js')
       copy_file 'semantic_ui.css', File.join(stylesheets, 'semantic_ui.css')
 
